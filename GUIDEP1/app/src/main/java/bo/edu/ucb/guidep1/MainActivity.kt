493fcb9dd4c4ad5874b8ab5381bf062a5fab86d3
ASDIO1 <diego.an.sanchez.a@gmail.com>
2022-02-28 19:37:47 -0400

    Guia 3: Obtener views desde codigo fuente
    - Se tuvo que modificar codigo porque la guia 3 esta desactualizada respecto al API 26
    
    - El cambio fue minimo

--- GUIDEP1/app/src/main/java/bo/edu/ucb/guidep1/MainActivity.kt
+++ GUIDEP1/app/src/main/java/bo/edu/ucb/guidep1/MainActivity.kt
@@ -2,8 +2,28 @@ package bo.edu.ucb.guidep1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.graphics.Color
+//import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
+//import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    // Para findViewById:
+    // Desde el API 26+  se necesita especificar el tipo de retorno
+    // Referencia: https://stackoverflow.com/questions/45267041/not-enough-information-to-infer-parameter-t-with-kotlin-and-android
+    private val redBtn : Button
+        get() = findViewById<Button>(R.id.btnRojo)
+
+    private val whiteBtn: Button
+        get() = findViewById<Button>(R.id.btnBlanco)
+
+    private val layoutPrincipal: LinearLayout
+        get() = findViewById<LinearLayout>(R.id.layoutMain)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
