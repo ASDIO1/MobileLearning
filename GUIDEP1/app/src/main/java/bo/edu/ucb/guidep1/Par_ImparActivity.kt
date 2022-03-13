941c144387656d50ced4eb5f8689c63e12ac605d
ASDIO1 <diego.an.sanchez.a@gmail.com>
2022-03-12 21:27:27 -0400

    Guia 4: Ej 3 -  Numero randomico generado para el juego
    - Se agregaron los views y junto a una funcion se inicializa el valor del numero de forma randomica desde Par_ImparActivity.kt

--- GUIDEP1/app/src/main/java/bo/edu/ucb/guidep1/Par_ImparActivity.kt
+++ GUIDEP1/app/src/main/java/bo/edu/ucb/guidep1/Par_ImparActivity.kt
@@ -2,10 +2,39 @@ package bo.edu.ucb.guidep1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlin.random.Random
 
 class Par_ImparActivity : AppCompatActivity() {
+
+    private val  contadorIncorrectosTxt : TextView
+        get() = findViewById(R.id.incorrectCounterText_ParImpar)
+    private val  contadorCorrectosTxt : TextView
+        get() = findViewById(R.id.correctCounterText_ParImpar)
+    private val  valueParImparTxt : TextView
+        get() = findViewById(R.id.valueTxt_ParImpar)
+    private val  parBtn : TextView
+        get() = findViewById(R.id.imparSelectBtn_ParImpar)
+    private val  imparBtn : TextView
+        get() = findViewById(R.id.parSelectBtn_ParImpar)
+
+    private var contadorIncorrectos = 0
+    private var contadorCorrectos = 0
+    private var valueParImpar = "0"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_par_impar)
+
+        valueParImpar = generateRandomNumber(0, 10)
+        valueParImparTxt.setText(valueParImpar)
+
+        valueParImparTxt.setOnClickListener{
+
+        }
+
+    }
+
+    private fun generateRandomNumber(startNumber : Int, endNumber : Int): String {
+       return Random.nextInt(startNumber, endNumber).toString()
     }
 }
