fd7920fafc847e5ffa712ac4b45669630544cac3
ASDIO1 <diego.an.sanchez.a@gmail.com>
2022-03-13 00:54:54 -0400

    Guia 4: Ej 3 -  Completado - EL juego es totalmente funcional
    - Se agregaron funciones para verificar si se acerto par o impar y para incrementar contadores y resetar el valor a adivinar
    
    - NOTA: Se evito hardcodear la mayoria de cosas, pero aun se puede refactorizar mas el codigo, pero para el proposito del aprender esta claro

--- GUIDEP1/app/src/main/java/bo/edu/ucb/guidep1/Par_ImparActivity.kt
+++ GUIDEP1/app/src/main/java/bo/edu/ucb/guidep1/Par_ImparActivity.kt
@@ -7,6 +7,7 @@ import kotlin.random.Random
 
 class Par_ImparActivity : AppCompatActivity() {
 
+    // VIEWS:
     private val  contadorIncorrectosTxt : TextView
         get() = findViewById(R.id.incorrectCounterText_ParImpar)
     private val  contadorCorrectosTxt : TextView
@@ -18,9 +19,12 @@ class Par_ImparActivity : AppCompatActivity() {
     private val  imparBtn : TextView
         get() = findViewById(R.id.parSelectBtn_ParImpar)
 
+    // VALUES:
     private var contadorIncorrectos = 0
     private var contadorCorrectos = 0
     private var valueParImpar = "0"
+
+    // METHODS:
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_par_impar)
@@ -28,13 +32,41 @@ class Par_ImparActivity : AppCompatActivity() {
         valueParImpar = generateRandomNumber(0, 10)
         valueParImparTxt.setText(valueParImpar)
 
-        valueParImparTxt.setOnClickListener{
+        imparBtn.setOnClickListener{
+            checkParImparAnswer(imparBtn.text.toString())
+            valueParImpar = generateRandomNumber(0, 10)
+            valueParImparTxt.setText(valueParImpar)
+        }
 
+        parBtn.setOnClickListener{
+            checkParImparAnswer(parBtn.text.toString())
+            valueParImpar = generateRandomNumber(0, 10)
+            valueParImparTxt.setText(valueParImpar)
         }
 
     }
 
     private fun generateRandomNumber(startNumber : Int, endNumber : Int): String {
        return Random.nextInt(startNumber, endNumber).toString()
+    }
+
+    private fun checkParImparAnswer(odd_or_even : String) {
+        val actualValue : String = isParImpar(valueParImparTxt.text.toString())
+        if (odd_or_even == actualValue){
+            this.contadorCorrectos = contadorCorrectos + 1
+            this.contadorCorrectosTxt.text = contadorCorrectos.toString()
+        } else {
+            this.contadorIncorrectos = contadorIncorrectos + 1
+            this.contadorIncorrectosTxt.text = contadorIncorrectos.toString()
+        }
+    }
+
+    private fun isParImpar(value : String) : String{
+        val intVaule = value.toInt()
+        if(intVaule % 2 === 0){
+            return getString(R.string.par_txt_parimpar)
+        } else {
+            return getString(R.string.impar_txt_parimpar)
+        }
     }
 }
