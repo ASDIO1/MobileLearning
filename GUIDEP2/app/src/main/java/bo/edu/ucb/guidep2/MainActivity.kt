73380aad3a608d0c5ee10fe6e8285f6a51ce7db4
ASDIO1 <diego.an.sanchez.a@gmail.com>
2022-03-27 16:38:58 -0400

    Guia 6: TSoporte para actionbar agregado
    - Hay error aun, porque el toolbar creado colisiona con el toolbar nativo por algun motivo

--- GUIDEP2/app/src/main/java/bo/edu/ucb/guidep2/MainActivity.kt
+++ GUIDEP2/app/src/main/java/bo/edu/ucb/guidep2/MainActivity.kt
@@ -1,11 +1,16 @@
 package bo.edu.ucb.guidep2
 
-import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity //antiguamente: android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar.let {
+            setSupportActionBar(findViewById(R.id.toolbar))
+        }
+        //setSupportActionBar(findViewById(R.id.toolbar)) //https://developer.android.com/training/appbar/setting-up
     }
 }
