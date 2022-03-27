c149cd8e963f19bfec2dacc5e9e543477ed1b6ca
ASDIO1 <diego.an.sanchez.a@gmail.com>
2022-03-27 17:05:34 -0400

    Guia 6: Toolbar custom - Completado Mejorado
    - COdigo fuente refactorizado, color y titulo del toolbar modificado
    
    - Se extrajo el toolbar a una variable y se redujo el boilerplate para el setSupportActionBar()
    
    - A la variable extraida se le agrego un titulo para validar que la toolbar nueva funciona bien
    
    - Se cambio el color del texo a blanco, en el xml del toolbar nuevo
    
    - NOTAS: Creat toolbar con el tag de un toolbar - Asegurarse de que el tema oculte el toolbar nativo NoActionBar - Si se extrae variable del toolbar escoger el tipo ToolBar de appCompat y no el de widget

--- GUIDEP2/app/src/main/java/bo/edu/ucb/guidep2/MainActivity.kt
+++ GUIDEP2/app/src/main/java/bo/edu/ucb/guidep2/MainActivity.kt
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity //antiguamente: android.support.
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myToolbar: androidx.appcompat.widget.Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        myToolbar = findViewById(R.id.toolbar)
+        myToolbar.title = "GuideP2 CustomToolbar"
+        setSupportActionBar(myToolbar) //https://developer.android.com/training/appbar/setting-up
+        /*
+        // Forma alargada:
         supportActionBar.let {
-            setSupportActionBar(findViewById(R.id.toolbar))
-        }
-        //setSupportActionBar(findViewById(R.id.toolbar)) //https://developer.android.com/training/appbar/setting-up
+            setSupportActionBar(my_toolbar)
+        }*/
     }
 }
