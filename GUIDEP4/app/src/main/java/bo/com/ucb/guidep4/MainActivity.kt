d141900be367525a9d3f037b7637c06e0157cc7e
ASDIO1 <diego.an.sanchez.a@gmail.com>
2022-05-28 18:46:24 -0400

    Guia 17 - Add Codigo fuente para activar swipe refresh

--- GUIDEP4/app/src/main/java/bo/com/ucb/guidep4/MainActivity.kt
+++ GUIDEP4/app/src/main/java/bo/com/ucb/guidep4/MainActivity.kt
@@ -1,11 +1,28 @@
 package bo.com.ucb.guidep4
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private val swipeToRefresLayout: androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+        get() = findViewById(R.id.swipeToRefresLayout)
+
+    private val  message_text_view : TextView
+        get() = findViewById(R.id.message_text_view)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        swipeToRefresLayout.setOnRefreshListener {
+            message_text_view.text = Random(100).nextInt(100).toString()
+            swipeToRefresLayout.isRefreshing = false
+            Log.d(TAG, message_text_view.text.toString())
+
+        }
     }
 }
