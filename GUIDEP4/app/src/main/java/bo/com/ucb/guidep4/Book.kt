c59a72e554f2431dda219e12bdc2fb89ecd9d3aa
ASDIO1 <diego.an.sanchez.a@gmail.com>
2022-05-30 12:39:41 -0400

    Guia 21 room inicio - importar librerias y entidad book

---
+++ GUIDEP4/app/src/main/java/bo/com/ucb/guidep4/Book.kt
@@ -0,0 +1,12 @@
+package bo.com.ucb.guidep4
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "book_table")
+class Book(@ColumnInfo(name = "title") var title: String) {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long = 0
+}
