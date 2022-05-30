a6ca2c0a8e35927b1e0e4279b92696bbee188a24
ASDIO1 <diego.an.sanchez.a@gmail.com>
2022-05-30 12:45:26 -0400

    Guia 21 - Add clase para definir metodos que consume un book

---
+++ GUIDEP4/app/src/main/java/bo/com/ucb/guidep4/BookRepository.kt
@@ -0,0 +1,12 @@
+package bo.com.ucb.guidep4
+
+class BookRepository(private val bookDao: IBookDao) {
+
+    suspend fun insert(book: Book) {
+        bookDao.insert(book)
+    }
+
+    fun getListBooks(): List<Book> {
+        return bookDao.getList()
+    }
+}
